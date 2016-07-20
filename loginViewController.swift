51519a71bf70ad62f0f71f33b59dba2f493b0adc
johnmontewhat <us4john@gmail.com>
2016-07-20 15:31:54 -0700

    first commit

---
+++ loginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  loginViewController.swift
+//  serviceApp
+//
+//  Created by John Montejano on 7/20/16.
+//  Copyright © 2016 John Montejano. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+class loginViewController: UIViewController {
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
